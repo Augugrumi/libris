3df7fbafe2d6ad4c6409a6505b2f0cb4fe198069
Davide Polonio <poloniodavide@gmail.com>
2017-04-28 19:00:43 +0200

    New dependencies & stuff

---
+++ src/main/java/it/polpetta/libris/IAbstractFactoryReverseSearchProvider.java
@@ -0,0 +1,9 @@
+package it.polpetta.libris;
+
+/**
+ * Created by davide on 28/04/17.
+ */
+public interface IReverseSearchProvider {
+
+    // I need a query builder here
+}
---
+++ src/main/java/it/polpetta/libris/ReverseImageSearch.java
@@ -0,0 +1,7 @@
+package it.polpetta.libris;
+
+/**
+ * Created by davide on 28/04/17.
+ */
+public class ReverseImageSearch {
+}
