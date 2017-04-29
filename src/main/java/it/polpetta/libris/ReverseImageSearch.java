affa8f5181e80a736d531b9637d6f9dce34a8e26
Davide Polonio <poloniodavide@gmail.com>
2017-04-29 18:52:00 +0200

    Fix minor compilation error

--- src/main/java/it/polpetta/libris/ReverseImageSearch.java
+++ src/main/java/it/polpetta/libris/ReverseImageSearch.java
@@ -1,6 +1,5 @@
 package it.polpetta.libris;
 
-import com.sun.istack.internal.NotNull;
 import it.polpetta.libris.google.GoogleAbstractFactory;
 
 /**
@@ -8,12 +7,10 @@ import it.polpetta.libris.google.GoogleAbstractFactory;
  */
 public class ReverseImageSearch {
 
-    @NotNull
     public static IAbstractFactoryReverseSearchProvider getGoogleServices() {
         return new GoogleAbstractFactory();
     }
 
-    // TODO set it as @NotNull
     public static IAbstractFactoryReverseSearchProvider getClarifaiServices() {
         return null;
     }
