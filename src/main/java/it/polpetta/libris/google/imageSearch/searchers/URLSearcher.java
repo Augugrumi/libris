35231f048cc22888a0b67df50adcee15dea0d7b4
mzanella <zanna0150@gmail.com>
2017-04-29 16:03:40 +0200

    changed parsing of links and added parsing for best guess, description, similar images and titles

--- src/main/java/it/polpetta/libris/google/imageSearch/searchers/URLSearcher.java
+++ src/main/java/it/polpetta/libris/google/imageSearch/searchers/URLSearcher.java
@@ -20,14 +20,17 @@ import java.util.ArrayList;
  */
 class URLSearcher implements ISearcher {
 
-    private URL link = null;
+    private static final String googleImageSearch = "https://www.google.com/searchbyimage?&image_url=";
+    private static final String userAgentProperty = "User-Agent";
+    private static final String userAgentValue =
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) " +
+            "Chrome/23.0.1271.97 Safari/537.11";
+
     private Coordinates location = null;
+    private URL link = null;
 
 
     URLSearcher (URL pathToImage, Coordinates location) {
-
-        final String googleImageSearch = "https://www.google.com/searchbyimage?&image_url=";
-
         try {
             this.link = new URL( googleImageSearch + pathToImage.toString());
 
@@ -44,11 +47,7 @@ class URLSearcher implements ISearcher {
         JSONObject res = new JSONObject();
 
         URLConnection toDownload = link.openConnection();
-        toDownload.setRequestProperty(
-                "User-Agent",
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) " +
-                "Chrome/23.0.1271.97 Safari/537.11"
-        );
+        toDownload.setRequestProperty(userAgentProperty, userAgentValue);
 
         BufferedReader in = new BufferedReader(new InputStreamReader(toDownload.getInputStream()));
 
@@ -70,49 +69,81 @@ class URLSearcher implements ISearcher {
 
         Elements body = parsedPage.body().children();
 
-
-        // LINKS
-        Elements links = body.select("div");
-        ArrayList<String> linkRes = new ArrayList<String>();
-
-        for (Element link : links) {
-
-            Elements gDiv = link.getElementsByClass("g");
-
-            for (Element singleLink : gDiv) {
-
-                Elements gDivLink = singleLink.select("a[href]");
-
-                for (Element uniqueLink : gDivLink) {
-
-                    uniqueLink.empty();
-                    if (!uniqueLink.hasClass("_Fmb ab_button") &&
-                            ! uniqueLink.hasClass("fl") &&
-                            ! uniqueLink.hasClass("bia") &&
-                            ! uniqueLink.hasClass("duf3") &&
-                            ! uniqueLink.hasClass("iu-card-header") &&
-                            ! uniqueLink.hasAttr("data-rtid") &&
-                            ! uniqueLink.hasAttr("data-ved")) {
-                        String toCheck = uniqueLink.attr("href");
-                        if (!linkRes.contains(toCheck)) {
-                            linkRes.add(toCheck);
-                        }
-
-                    }
-
-                }
-            }
-
-        }
-
-        res.put("links", linkRes);
-
-        // END LINKS
-
-        // DESCRIPTIONS
-
-        // END DESCRIPTIONS
+        res.put("best_guess", retrieveBestGuessFromHTML(body));
+        res.put("links", retrieveLinksFromHTML(body));
+        res.put("descriptions", retrieveDescriptionFromHTML(body));
+        res.put("titles", retrieveTitleFromHTML(body));
+        res.put("similar_images", retrieveSimilarImageFromHTML(body));
 
         return res;
+    }
+
+    private String retrieveBestGuessFromHTML(Elements body) {
+        String bestGuessRes;
+        Elements bestGuesses = body.select("a._gUb");
+        bestGuessRes = bestGuesses.first().text();
+        return bestGuessRes;
+    }
+
+    private ArrayList<String> retrieveLinksFromHTML(Elements body) {
+        ArrayList<String> linkRes = new ArrayList<String>();
+        Elements links = body.select("div.g");
+        links = links.select("a[href]");
+        for (Element link : links){
+            if (!link.hasClass("_Fmb ab_button") &&
+                    ! link.hasClass("fl") &&
+                    ! link.hasClass("bia") &&
+                    ! link.hasClass("duf3") &&
+                    ! link.hasClass("iu-card-header") &&
+                    ! link.hasAttr("data-rtid") &&
+                    ! link.hasAttr("data-ved")) {
+                String toCheck = link.attr("href");
+                if (!linkRes.contains(toCheck)) {
+                    try {
+                        new URL(toCheck);
+                        linkRes.add(toCheck);
+                    } catch (MalformedURLException e) {}
+                }
+
+            }
+        }
+        return linkRes;
+    }
+
+    private ArrayList<String> retrieveDescriptionFromHTML(Elements body) {
+        ArrayList<String> descRes = new ArrayList<String>();
+        Elements descriptions = body.select("span.st");
+
+        for (Element desc : descriptions)
+            descRes.add(desc.text());
+
+        return descRes;
+    }
+
+    private ArrayList<String> retrieveTitleFromHTML(Elements body) {
+        ArrayList<String> titleRes = new ArrayList<String>();
+        Elements titles = body.select("h3.r");
+
+        for (Element title : titles)
+            titleRes.add(title.text());
+
+        return titleRes;
+    }
+
+    private ArrayList<String> retrieveSimilarImageFromHTML(Elements body) {
+        ArrayList<String> imageRes = new ArrayList<String>();
+        Elements images = body.select("div.rg_meta");
+
+        JSONObject jsImage;
+        String imageUrl;
+        for (Element image : images){
+            jsImage = new JSONObject(image.text());
+            imageUrl = jsImage.getString("ou");
+            try {
+                new URL(imageUrl);
+                imageRes.add(imageUrl);
+            } catch (MalformedURLException e) {}
+        }
+        return imageRes;
     }
 }
