530850e24211f68cb0a13ba63071605bf8b05190
Davide Polonio <poloniodavide@gmail.com>
2017-05-03 18:33:29 +0200

    New empty SSLObtainer

---
+++ src/main/java/it/polpetta/libris/utils/webSocket/SSL/LetsEncryptSSLObtainer.java
@@ -0,0 +1,13 @@
+package it.polpetta.libris.utils.webSocket.SSL;
+
+import javax.net.ssl.SSLContext;
+
+/**
+ * Created by davide on 03/05/17.
+ */
+public class LetsEncryptSSLObtainer implements ISSLContextObtainer {
+
+    public SSLContext getSSLContext () {
+        return null;
+    }
+}
