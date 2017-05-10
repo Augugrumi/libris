8d129ed9fe7fa1a228c627b9a97d447ebf11ee81
Davide Polonio <poloniodavide@gmail.com>
2017-05-10 16:34:53 +0200

    Clean up

--- src/main/java/it/polpetta/libris/image/azure/imageRecognition/AzureImageSearchResult.java
+++ src/main/java/it/polpetta/libris/image/azure/imageRecognition/AzureImageSearchResult.java
@@ -1,14 +1,7 @@
 package it.polpetta.libris.image.azure.imageRecognition;
 
-import it.polpetta.libris.contract.IQueryBuilder;
-import it.polpetta.libris.contract.ISearcher;
 import it.polpetta.libris.image.azure.contract.IAzureImageSearchResult;
-import it.polpetta.libris.image.google.contract.IGoogleImageSearchResult;
-import it.polpetta.libris.image.google.hmtlImageSearch.GoogleImageSearchResult;
-import it.polpetta.libris.utils.Coordinates;
 
-import java.io.File;
-import java.net.URL;
 import java.util.ArrayList;
 
 /**
