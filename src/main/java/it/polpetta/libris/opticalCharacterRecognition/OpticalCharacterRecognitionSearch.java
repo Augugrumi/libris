b0fcaaf6f07bc86df373aa6ed397dbc243d73c1a
Davide Polonio <poloniodavide@gmail.com>
2017-05-12 14:07:50 +0200

    Set OpticalCharacterRecognitionSearch constructor private

--- src/main/java/it/polpetta/libris/opticalCharacterRecognition/OpticalCharacterRecognitionSearch.java
+++ src/main/java/it/polpetta/libris/opticalCharacterRecognition/OpticalCharacterRecognitionSearch.java
@@ -7,6 +7,11 @@ import it.polpetta.libris.opticalCharacterRecognition.azure.contract.IAbstractAz
  * Created by zanna on 11/05/17.
  */
 public class OpticalCharacterRecognitionSearch {
+
+    private OpticalCharacterRecognitionSearch() {
+
+    }
+
     public static IAbstractAzureOcrFactoryReverseSearchProvider getAzureServices(String subscriptionKey) {
         return new AzureAbstractFactory(subscriptionKey);
     }
