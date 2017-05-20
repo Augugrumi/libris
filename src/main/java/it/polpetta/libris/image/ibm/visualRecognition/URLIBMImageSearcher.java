19be7f8c480a43ce2c8475d37444c5aa2ae4e309
mzanella <zanna0150@gmail.com>
2017-05-20 10:10:13 +0200

    Update date version watson

--- src/main/java/it/polpetta/libris/image/ibm/visualRecognition/URLIBMImageSearcher.java
+++ src/main/java/it/polpetta/libris/image/ibm/visualRecognition/URLIBMImageSearcher.java
@@ -69,7 +69,7 @@ public class URLIBMImageSearcher extends AbstractURLSearcher implements IIBMImag
     @Override
     public IIBMImageSearchResult search() throws IOException {
         VisualRecognition service =
-                new VisualRecognition("2017-05-20", subscriptionKey);
+                new VisualRecognition("2017-05-18T19:50Z", subscriptionKey);
 
         ClassifyImagesOptions options1 = new ClassifyImagesOptions.Builder()
                 .url(link.toString())
